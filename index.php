a02f538a599d232041564e0907a1aedb988de561
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-20 11:14:42 +0330

    feat : add the index page and add the input and button

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="todo_name" placeholder="Enter your todo">
+        <button>New todo</button>
+    </form>
+</body>
+</html>
