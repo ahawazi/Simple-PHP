3db26114d218ebb45859a451746829b038f53311
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-20 15:58:44 +0330

    feat : add the index

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php
 
+$todos = [];
+if (file_exists('todo.json')) {
+    $json = file_get_contents('todo.json');
+    $todos = json_decode($json, true);
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <form action="" method="post">
+    <form action="newtodo.php" method="post">
         <input type="text" name="todo_name" placeholder="Enter your todo">
         <button>New todo</button>
     </form>
+    <br>
+
+    <?php foreach ($todos as $todoName => $todo): ?>
+        <div style="margin-bottom: 20px">
+            <input type="checkbox" <?php echo $todo['completed'] ? 'cheched' : '' ?>>
+            <?php echo $todoName ?>
+        </div>
+    <?php endforeach ?>
+
 </body>
+
 </html>
