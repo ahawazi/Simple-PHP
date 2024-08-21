96eec0692fbd3dc26efd7601c688c03980dc1a62
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-21 18:19:11 +0330

    fix : delete the html and include the components in the index page

--- simple_CRUD/index.php
+++ simple_CRUD/index.php
@@ -3,18 +3,8 @@
 require 'users.php';
 $users = getUsers();
 
+include 'partials/header.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>simple php crud</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 <table class="table">
     <thead>
@@ -46,5 +36,4 @@ $users = getUsers();
     </tbody>
 </table>
 
-</body>
-</html>
+<?php include 'partials/footer.php' ?>
