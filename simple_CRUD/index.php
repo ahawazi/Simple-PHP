ad0e03f6604849e0ffbbec8ae065f4cf487f252a
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-21 16:14:35 +0330

    feat : create table for users in the index

--- simple_CRUD/index.php
+++ simple_CRUD/index.php
@@ -16,5 +16,35 @@ $users = getUsers();
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
+<table class="table">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>UserName</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Website</th>
+        <th>Actions</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <?php foreach ($users as $user): ?>
+    <tr>
+        <td><?php echo $user['name'] ?></td>
+        <td><?php echo $user['username'] ?></td>
+        <td><?php echo $user['email'] ?></td>
+        <td><?php echo $user['phone'] ?></td>
+        <td><?php echo $user['website'] ?></td>
+        <td>
+            <a href="view.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-info">View</a>
+            <a href="update.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-secondary">Update</a>
+            <a href="delete.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-danger">Delete</a>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
 </body>
 </html>
