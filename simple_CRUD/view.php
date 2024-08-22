86c9d3647f3a27667697cb41f671ab8d163fafe9
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-22 12:28:26 +0330

    feat : include the not_found and show the name

--- simple_CRUD/view.php
+++ simple_CRUD/view.php
@@ -2,36 +2,55 @@
 
 require __DIR__.'/users.php';
 
+if (!isset($_GET['id'])) {
+    include "partials/not_found.php";
+    exit;
+}
 $userId = $_GET['id'];
 
 $user = getUserById($userId);
 
+if (!isset($user)) {
+    include "partials/not_found.php";
+    exit;
+}
+
 include 'partials/header.php'
 ?>
 
-<table class="table">
-    <tbody>
-        <tr>
-            <th>Name:</th>
-            <td><?php echo $user['name'] ?></td>
-        </tr>
-        <tr>
-            <th>UserName:</th>
-            <td><?php echo $user['username'] ?></td>
-        </tr>
-        <tr>
-            <th>Email:</th>
-            <td><?php echo $user['email'] ?></td>
-        </tr>
-        <tr>
-            <th>Phone:</th>
-            <td><?php echo $user['phone'] ?></td>
-        </tr>
-        <tr>
-            <th>Website:</th>
-            <td><?php echo $user['website'] ?></td>
-        </tr>
-    </tbody>
-</table>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>
+                View user: <b><?php echo $user['name']?></b>
+            </h3>
+        </div>
+        <table class="table">
+            <tbody>
+            <tr>
+                <th>Name:</th>
+                <td><?php echo $user['name'] ?></td>
+            </tr>
+            <tr>
+                <th>UserName:</th>
+                <td><?php echo $user['username'] ?></td>
+            </tr>
+            <tr>
+                <th>Email:</th>
+                <td><?php echo $user['email'] ?></td>
+            </tr>
+            <tr>
+                <th>Phone:</th>
+                <td><?php echo $user['phone'] ?></td>
+            </tr>
+            <tr>
+                <th>Website:</th>
+                <td><?php echo $user['website'] ?></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
 
 <?php include 'partials/footer.php' ?>
