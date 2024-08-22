9bc77d82ad60e7101306b0e7089564f04d96c564
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-22 15:08:40 +0330

    feat : add the update

--- simple_CRUD/update.php
+++ simple_CRUD/update.php
@@ -15,42 +15,54 @@ if (!isset($user)) {
     include 'partials/not_found.php';
     exit;
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    updateUser($_POST, $userId);
+
+    header("Location: index.php");
+}
+
 ?>
 
-<div class="">
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>Update User <b><?php echo $user['name']?></b></h3>
+        </div>
+        <div class="card-body">
+            <form method="post" enctype="multipart/form-data" action="#">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="name" value="<?php echo $user['name'] ?>" class="form-control">
+                </div>
 
+                <div class="form-group">
+                    <label>UserName</label>
+                    <input name="username" value="<?php echo $user['username'] ?>" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Email</label>
+                    <input name="email" value="<?php echo $user['email'] ?>" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Phone</label>
+                    <input name="phone" value="<?php echo $user['phone'] ?>" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Website</label>
+                    <input name="website" value="<?php echo $user['website'] ?>" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label>Image</label>
+                    <input name="picture" type="image" class="form-control-file">
+                </div>
+
+                <button class="btn btn-success">Submit</button>
+            </form>
+        </div>
+    </div>
 </div>
-<form method="post" enctype="multipart/form-data" action="#">
-    <div class="form-group">
-        <label>Name</label>
-        <input type="name" value="<?php echo $user['name'] ?>" class="form-control">
-    </div>
-
-    <div class="form-group">
-        <label>UserName</label>
-        <input name="username" value="<?php echo $user['username'] ?>" class="form-control">
-    </div>
-
-    <div class="form-group">
-        <label>Email</label>
-        <input name="email" value="<?php echo $user['email'] ?>" class="form-control">
-    </div>
-
-    <div class="form-group">
-        <label>Phone</label>
-        <input name="phone" value="<?php echo $user['phone'] ?>" class="form-control">
-    </div>
-
-    <div class="form-group">
-        <label>Website</label>
-        <input name="website" value="<?php echo $user['website'] ?>" class="form-control">
-    </div>
-    
-    <div class="form-group">
-        <label>Image</label>
-        <input name="picture" type="image" class="form-control-file">
-    </div>
-
-    <button class="btn btn-success">Submit</button>
-
-</form>
