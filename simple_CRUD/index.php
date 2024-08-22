967b91743604a86c897a7dcdb36a3a63568fa53c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-22 12:27:09 +0330

    fix : move the table to the container in the index page

--- simple_CRUD/index.php
+++ simple_CRUD/index.php
@@ -6,34 +6,36 @@ $users = getUsers();
 include 'partials/header.php';
 ?>
 
-<table class="table">
-    <thead>
-    <tr>
-        <th>Name</th>
-        <th>UserName</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>Website</th>
-        <th>Actions</th>
-    </tr>
-    </thead>
+<div class="container">
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>UserName</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
 
-    <tbody>
-    <?php foreach ($users as $user): ?>
-    <tr>
-        <td><?php echo $user['name'] ?></td>
-        <td><?php echo $user['username'] ?></td>
-        <td><?php echo $user['email'] ?></td>
-        <td><?php echo $user['phone'] ?></td>
-        <td><?php echo $user['website'] ?></td>
-        <td>
-            <a href="view.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-info">View</a>
-            <a href="update.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-secondary">Update</a>
-            <a href="delete.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-danger">Delete</a>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+        <tbody>
+        <?php foreach ($users as $user): ?>
+            <tr>
+                <td><?php echo $user['name'] ?></td>
+                <td><?php echo $user['username'] ?></td>
+                <td><?php echo $user['email'] ?></td>
+                <td><?php echo $user['phone'] ?></td>
+                <td><?php echo $user['website'] ?></td>
+                <td>
+                    <a href="view.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-info">View</a>
+                    <a href="update.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-secondary">Update</a>
+                    <a href="delete.php?id=<?php echo $user['id']?>" class="btn btn-sm btn-outline-danger">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 
 <?php include 'partials/footer.php' ?>
