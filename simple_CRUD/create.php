2e3eaceed438892fb0b405097ec2666e00b9c403
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-24 00:26:46 +0330

    feat : add the create user page

---
+++ simple_CRUD/create.php
@@ -0,0 +1,24 @@
+<?php
+include 'partials/header.php';
+require __DIR__ . '/users/users.php';
+
+$user = [
+    'id' => '',
+    'name' => '',
+    'username' => '',
+    'email' => '',
+    'phone' => '',
+    'website' => ','
+];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user = createUser($_POST);
+
+    uploadImage($_FILES['picture'], $user);
+
+    header("Location: index.php");
+}
+
+?>
+
+<?php include '_form.php' ?>
