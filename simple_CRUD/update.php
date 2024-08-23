7adbb6c18488513e7fef33e2332308c603b7c524
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-24 00:27:59 +0330

    ref : update page

--- simple_CRUD/update.php
+++ simple_CRUD/update.php
@@ -17,56 +17,13 @@ if (!isset($user)) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    updateUser($_POST, $userId);
+    $user = updateUser($_POST, $userId);
 
-    if (isset($_FILES['picture'])) {
-        move_uploaded_file($_FILES['picture']['tmp_name'], __DIR__ . "/images/$userId.jpg");
-    }
+    uploadImage($_FILES['picture'], $user);
 
     header("Location: index.php");
 }
 
 ?>
 
-<div class="container">
-    <div class="card">
-        <div class="card-header">
-            <h3>Update User <b><?php echo $user['name']?></b></h3>
-        </div>
-        <div class="card-body">
-            <form method="post" enctype="multipart/form-data" action="#">
-                <div class="form-group">
-                    <label>Name</label>
-                    <input name="name" value="<?php echo $user['name'] ?>" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label>UserName</label>
-                    <input name="username" value="<?php echo $user['username'] ?>" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label>Email</label>
-                    <input name="email" value="<?php echo $user['email'] ?>" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label>Phone</label>
-                    <input name="phone" value="<?php echo $user['phone'] ?>" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label>Website</label>
-                    <input name="website" value="<?php echo $user['website'] ?>" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label>Image</label>
-                    <input name="picture" type="image" class="form-control-file">
-                </div>
-
-                <button class="btn btn-success">Submit</button>
-            </form>
-        </div>
-    </div>
-</div>
+<?php include '_form.php' ?>
