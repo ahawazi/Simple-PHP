d0ab4ca6739d3dfc0f5e747d1cb1a7bdcca40d62
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-24 16:17:19 +0330

    fit : index for oop

---
+++ oop/index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Person
+{
+    public string $name;
+    public int $age;
+    public float $salary;
+
+    public function __construct($name, $age, $salary)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
+    }
+}
+
+$p = new Person('John Doe', '18', null);
+
+echo '<pre>';
+var_dump($p);
+echo '</pre>';
